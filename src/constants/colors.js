5985fa65a0d15f1e47274bad09f4f94657070d34
jieunn <jieun776121@gmail.com>
2024-09-07 18:02:46 +0900

    feat: color 추가 및 배경색 지정

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,9 +1,8 @@
 export const colors = {
-	'Blue-100': '#E0F8FF',
-	'Blue-200': '#10C7F9',
-	'Blue-300': '#00AEFF',
-	'Blue-400': '#0095FF',
-	'Blue-500': '#2563FF',
-	Black: '#000000',
-	Gray: '#666666',
+  "Gray-100": "#F1F3F9",
+  "Gray-200": "#A8A8A9",
+  "Gray-300": "#565A64",
+  "Gray-400": "#27292F",
+  "Blue-100": "#007AFF",
+  Black: "#000000",
 };
