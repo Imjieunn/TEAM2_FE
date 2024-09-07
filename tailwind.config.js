80633d2069922d8474fe459507ada3947dc2a583
jieunn <jieun776121@gmail.com>
2024-09-07 16:35:39 +0900

    chore: tailwind setting

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import { colors } from "./src/constants/colors";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: colors,
+      fontFamily: {
+        pretendardLight: "light",
+        pretendardMedium: "medium",
+        pretendardSemibold: "semiBold",
+        pretendardBold: "bold",
+      },
+
+      backgroundColor: {
+        "custom-blue": "rgba(37, 99, 255, 0.9)",
+      },
+    },
+  },
+  plugins: [],
+};
